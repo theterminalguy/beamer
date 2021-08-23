710636d3500b7fa97bdaa316658bc7a15e15b694
theterminalguy <damiansimonpeter@gmail.com>
2021-08-23 00:46:40 -0400

    Validate template name

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/ls-simon-peter-damian/beamer"
 )
 
 func main() {
-	//beamer.Gen("BigQueryToDatastore")
 	if len(os.Args) < 2 {
 		showHelp()
 	}
 	switch os.Args[1] {
 	case "gen":
-		fmt.Println("Running `gen` command", os.Args[2])
+		validateTemplateName(os.Args[2])
+		beamer.Gen(os.Args[2])
 	case "run":
-		fmt.Println("Running `run` command", os.Args[2])
+		validateTemplateName(os.Args[2])
+		beamer.Run(os.Args[2])
 	default:
 		showHelp()
 	}
@@ -24,4 +27,11 @@ func showHelp() {
 	text := "usage: beamer <command> <args>\n\nThese are the only two commands available:\n\tgen\tgenerates a new job config\n\trun\truns the generated job config on GCP\n\nExamples:\n\t- Generates a job config for BigQueryToDatastore\n\t$ beamer gen BigQueryToDatastore\n\n\t- Run the job config for BigQueryToDatastore migration\n\t$ beamer run BigQueryToDatastore"
 	fmt.Println(text)
 	os.Exit(0)
+}
+
+func validateTemplateName(name string) {
+	if name == "" {
+		fmt.Println("Please provide the template name. See `beamer help` for more info.")
+		os.Exit(64)
+	}
 }
